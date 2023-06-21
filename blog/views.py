0eb3d86ba1cb507460c2cd668d2d4caf184beb08
dawid <piatek06892@wp.pl>
2023-06-21 18:57:20 +0200

    gotowy projekt2

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,11 @@ from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog\post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog\ost_detail.html', {'post': post})
 
 def error_404_view(request, exception):
     data = {"name": 'Blog dla programistów'}
